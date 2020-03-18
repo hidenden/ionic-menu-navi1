9c7b9e56f1674a90c1b4cdab4009276186938da9
Hidenari Miwa <hidenari.miwa@mac.com>
2020-03-18 12:06:08 +0900

    Push機能をService化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
+import { PushService } from './push.service';
 
 @Component({
   selector: 'app-root',
@@ -51,7 +51,7 @@ export class AppComponent implements OnInit {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private push: Push,
+    private pushService: PushService,
     private router: Router,
     private ngZone: NgZone,
   ) {
@@ -78,59 +78,6 @@ export class AppComponent implements OnInit {
       console.warn('Push notifications not initialized.  Run in physical device');
       return;
     }
-    const options: PushOptions = {
-      android: {
-        senderID: 'SomeSenderID',
-        forceShow: 'true'
-      },
-      ios: {
-        alert: 'true',
-        badge: 'true',
-        sound: 'true',
-      },
-      windows: {}
-    }
-    const pushObject: PushObject = this.push.init(options);
-    this.preparePushNotification(pushObject);
-    this.push.hasPermission().then( perm => {
-      console.log(`PushNotification permission: ${perm.isEnabled}`);
-      if (perm.isEnabled) {
-        console.log('Have Push permission');
-      } else {
-        console.log('Not have Push permission');
-      }
-    });
+    this.pushService.initialize();
   }
-
-  pushNavigation() {
-    console.log('Navigate to folder/Index');
-    this.ngZone.run(() => {
-      this.router.navigate(['folder', 'Inbox']);
-    });
-  }
-
-  preparePushNotification(pushObject: PushObject) {
-    pushObject.on('registration').subscribe((data: any) => {
-      console.log('device token -> ' + data.registrationId);
-      console.log('registratio type -> ' + data.registrationType);
-      // TODO - send device token to server
-    });
-
-    pushObject.on('notification').subscribe((data: any) => {
-      console.log('message -> ' + data.message);
-      // if user using app and push notification comes
-      if (data.additionalData.foreground) {
-        console.log('Received in foreground');
-      } else if (data.additionalData.coldstart) {
-        console.log('Push notification clicked');
-        this.pushNavigation();
-        // console.log('ADD NAVIGATION HERE');
-      } else {
-        console.log('App returned from background');
-      }
-    });
-
-    pushObject.on('error').subscribe(error => console.error('Error with Push plugin' + error));
-  }
-
 }
--- src/app/push.service.ts
+++ src/app/push.service.ts
@@ -1,9 +1,133 @@
 import { Injectable } from '@angular/core';
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
+  deviceToken: string;
+  registrationType: string;
+  badge: number = 0;
+  pushObject: PushObject;
 
-  constructor() { }
+  constructor(
+    private push: Push,
+  ) { }
+
+  initialize() {
+    const options: PushOptions = {
+      android: {
+        senderID: 'SomeSenderID',
+        forceShow: 'true'
+      },
+      ios: {
+        alert: 'true',
+        badge: 'true',
+        sound: 'true',
+      },
+      windows: {}
+    }
+    this.pushObject = this.push.init(options);
+
+    this.preparePushNotification();
+
+    this.push.hasPermission().then( perm => {
+      console.log(`PushNotification permission: ${perm.isEnabled}`);
+      if (perm.isEnabled) {
+        console.log('Have Push permission');
+      } else {
+        console.log('Not have Push permission');
+      }
+    });    
+  }
+
+  private preparePushNotification() : void {
+
+    this.pushObject.on('registration').subscribe((data: any) => {
+      this.updateToken(data.registrationId, data.registrationType);
+    });
+
+    this.pushObject.on('notification').subscribe((data: any) => {
+      console.log('message -> ' + data.message);
+      // if user using app and push notification comes
+      this.refreshBadge();
+
+      if (data.additionalData.foreground) {
+        this.foregroundAction(data);
+      } else if (data.additionalData.coldstart) {
+        this.coldstartAction(data);
+      } else {
+        this.frombackgroundAction(data);
+      }
+    });
+
+    this.pushObject.on('error').subscribe(error => {
+      this.errorAction(error);
+    });
+  }
+
+  private foregroundAction(data: any): void {
+    console.log('Received in foreground');
+  }
+
+  private coldstartAction(data: any): void {
+    console.log('Push notification clicked');
+    this.pushNavigation();
+    // console.log('ADD NAVIGATION HERE');
+  }
+
+  private frombackgroundAction(data: any): void {
+    console.log('App returned from background');
+  }
+
+  private errorAction(error: any) : void {
+    console.error('Error with Push plugin' + error);
+  }
+
+  private pushNavigation(): void {
+    console.log('Navigate to folder/Index');
+    /* 
+    this.ngZone.run(() => {
+      this.router.navigate(['folder', 'Inbox']);
+    });
+    */
+   this.setBadge(0);
+  }
+
+  getToken(): string{
+    return this.deviceToken;
+  }
+
+  private updateToken(token: string, registType: string) : void {
+    this.deviceToken = token;
+    this.registrationType = registType;
+
+    console.log('device token -> ' + this.deviceToken);
+    console.log('registratio type -> ' + this.registrationType);
+
+    console.log('--- Send a device token to the server at here ---');
+  }
+
+  getBadge(): number {
+    return this.badge;
+  }
+
+  private refreshBadge() : void {
+    this.pushObject.getApplicationIconBadgeNumber().then((num) => {
+      this.badge = num;
+      console.log('Badge number refresh:' + this.badge);
+    });
+  }
+
+  setBadge(num: number) {
+    this.pushObject.setApplicationIconBadgeNumber(num).then(() => {
+      this.badge = num;
+      console.log('Badge number is set:' + this.badge);
+    });
+  }
+
+  clearBadge(): void {
+    this.setBadge(0);
+  }
+
 }
